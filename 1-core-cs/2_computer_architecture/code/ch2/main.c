3b110ac39440b8811debb045120ddce1d6d0a6f9
Adam <asoliyev13@gmail.com>
2022-11-26 11:47:48 -0600

    added solutions link

--- 1-core-cs/2_computer_architecture/code/ch2/main.c
+++ 1-core-cs/2_computer_architecture/code/ch2/main.c
@@ -13,58 +13,6 @@ void _print(int a, char* str) {
   printf("\n");
 }
 
-//////////////////////////////////////////////////////////////////////////////
-void inplace_swap(int *x, int *y) {
-  // TODO: seems can't handle 0
-  
-  *y = *x ^ *y;
-  *x = *x ^ *y;
-  *y = *x ^ *y;
-}
-
-void reverse_array(int a[], int cnt) {
-  int first, last;
-  for (first = 0, last = cnt - 1; first < last; first++, last--) {
-    inplace_swap(&a[first], &a[last]);
-  }
-}
-
-void test__inplace_swap() {
-  
-  // initialize variables
-  int a = -23, b = 23, c = 9, d = 23492;
-
-  // test
-  inplace_swap(&a, &b);
-  assert(a = 23);
-  assert(b = -23);
-
-  inplace_swap(&c, &d);
-  assert(c = 23492);
-  assert(d = 9);
-
-  _print(1, "test__inplace_swap()");
-}
-
-void test__reverse_array() {
-  // initialize variables
-  int a[4] = {-2, 4, 8, 1}; 
-  int b[5] = {-2, 4, 8, 1, 9}; 
-  
-  // test
-  reverse_array(a, 4);
-  assert(a[0] = 1);
-  assert(a[2] = 4);
-  assert(a[3] = -2);
-
-  reverse_array(b, 5);
-  assert(b[0] = 9);
-  assert(b[2] = 8);
-  assert(b[4] = -2);
-
-  _print(1, "test__reverse_array()");
-}
-//////////////////////////////////////////////////////////////////////////////
 
 //////////////////////////////////////////////////////////////////////////////
 typedef unsigned char *byte_pointer;
@@ -102,9 +50,110 @@ void test__show_bytes(int val) {
 //////////////////////////////////////////////////////////////////////////////
 
 
+//////////////////////////////////////////////////////////////////////////////
+// 2.10
+void inplace_swap(int *x, int *y) {
+  // TODO: seems can't handle 0
+  
+  *y = *x ^ *y;
+  *x = *x ^ *y;
+  *y = *x ^ *y;
+}
+
+void test__inplace_swap() {
+  // initialize variables
+  int a = -23, b = 23, c = 9, d = 23492;
+
+  // test
+  inplace_swap(&a, &b);
+  assert(a = 23);
+  assert(b = -23);
+
+  inplace_swap(&c, &d);
+  assert(c = 23492);
+  assert(d = 9);
+
+  _print(1, "test__inplace_swap()");
+}
+
+// 2.11
+void reverse_array(int a[], int cnt) {
+  int first, last;
+  for (first = 0, last = cnt - 1; first < last; first++, last--) {
+    inplace_swap(&a[first], &a[last]);
+  }
+}
+
+void test__reverse_array() {
+  // initialize variables
+  int a[4] = {-2, 4, 8, 1}; 
+  int b[5] = {-2, 4, 8, 1, 9}; 
+  
+  // test
+  reverse_array(a, 4);
+  assert(a[0] = 1);
+  assert(a[2] = 4);
+  assert(a[3] = -2);
+
+  reverse_array(b, 5);
+  assert(b[0] = 9);
+  assert(b[2] = 8);
+  assert(b[4] = -2);
+
+  _print(1, "test__reverse_array()");
+}
+
+// 2.12
+void custom_bit_mask(int x) {
+  printf("*** 2.12 ***\n");
+  printf("x: %d\n", x);
+  show_int(x);
+ 
+  // A
+  int a = x & 0xFF;
+  printf("x & 0xFF: ");
+  show_int(a);
+
+  // B
+  int b = x ^ (~0xFF);
+  printf("x ^ (~0xFF): ");
+  show_int(b);
+
+  // C
+  int c = x | 0xFF;
+  printf("x | 0xFF: ");
+  show_int(c);
+}
+
+// 2.13
+int bis(int x, int m) { // bit set
+  return x | m;
+}
+int bic(int x, int m) { // bit clear
+  return x & (~m);
+}
+int bool_or(int x, int y) {
+  return bis(x, y);
+}
+
+int bool_xor(int x, int y) {
+  // x ^ y = (x & ~y) | (~x & y)
+  
+  return bis(bic(x, y), bic(y, x));
+}
+//////////////////////////////////////////////////////////////////////////////
+
 int main() {
+  /*
   test__show_bytes(43);
   test__inplace_swap();
   test__reverse_array();
+  */
+  // custom_bit_mask(0x65C9F2AB);
+  int b = 0x46;
+  show_int(~b);
+  show_int(!b);
+  show_int(!!b);
+
   return 0;
 }
