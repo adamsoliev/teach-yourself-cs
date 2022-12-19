201fe6b9cdd1148be63376c848136bbd03a913f0
Adam <asoliyev13@gmail.com>
2022-12-19 11:47:41 -0600

    2.65

--- 1-core-cs/2_computer_architecture/code/ch2.c
+++ 1-core-cs/2_computer_architecture/code/ch2.c
@@ -14,10 +14,35 @@ int int_sifts_are_arithmetic()
   return !(num ^ (num >> 1)); // uses a ^ a = 0 property
 }
 
+// 2.65 ◆◆◆◆
+// Write code to implement the following function:
+// /* Return 1 when x contains an odd number of 1s; 0 otherwise.
+// Assume w=32 */
+// int odd_ones(unsigned x);
+// Your function should follow the bit-level integer coding rules (page 164),
+// except that you may assume that data type int has w = 32 bits.
+// Your code should contain a total of at most 12 arithmetic, bitwise, and logical
+// operations.
+int odd_ones(unsigned x)
+{
+  // 0xffffffff
+  x ^= x >> 16;
+  x ^= x >> 8;
+  x ^= x >> 4;
+  x ^= x >> 2;
+  x ^= x >> 1;
+  x &= 0x1;
+  return x;
+}
+
+
 int main(int argc, char *argv[])
 {
   // 2.62 ◆◆◆
-  assert(int_sifts_are_arithmetic());
+  // assert(int_sifts_are_arithmetic());
 
+  // 2.65 ◆◆◆◆
+  // assert(odd_ones(0x10101011));
+  // assert(!odd_ones(0x01010101));
   return 0;
 }
