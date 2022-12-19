d7fa2251e7abacd0288fb546557b0849331bd68a
Adam <asoliyev13@gmail.com>
2022-12-19 11:35:57 -0600

    2.62

--- 1-core-cs/2_computer_architecture/code/ch2.c
+++ 1-core-cs/2_computer_architecture/code/ch2.c
@@ -3,8 +3,21 @@
 #include <stdio.h>
 #include "utils.h"
 
-int main()
+// 2.62 ◆◆◆
+// Write a function int_shifts_are_arithmetic() that yields 1 when run on a
+// machine that uses arithmetic right shifts for data type int and yields 0 otherwise.
+// Your code should work on a machine with any word size. Test your code on several
+// machines.
+int int_sifts_are_arithmetic()
 {
+  int num = -1;               // 0xFFFFFFFF
+  return !(num ^ (num >> 1)); // uses a ^ a = 0 property
+}
+
+int main(int argc, char *argv[])
+{
+  // 2.62 ◆◆◆
+  assert(int_sifts_are_arithmetic());
 
   return 0;
 }
