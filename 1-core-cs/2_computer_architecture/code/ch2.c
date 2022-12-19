ad0e5c6eee57557cb9a757402cd53803bc412edb
Adam <asoliyev13@gmail.com>
2022-12-19 10:33:07 -0600

    added some utils functions and refactored the file

--- 1-core-cs/2_computer_architecture/code/ch2.c
+++ 1-core-cs/2_computer_architecture/code/ch2.c
@@ -2,7 +2,28 @@
 #include <assert.h>
 #include <stdio.h>
 
+
 // ----------------------------- UTILS -----------------------------
+
+// helper for show_type_sizes()
+// str - message string (e.g., 'size of int')
+//
+void print2c_la_ra(char *str, size_t size) {
+  printf("%-15s %2zu\n", str, size);
+}
+
+// prints size of a given C type 
+void show_type_sizes() {
+  print2c_la_ra("size of char:", sizeof(char));
+  print2c_la_ra("size of short:", sizeof(short));
+  print2c_la_ra("size of int:", sizeof(int));
+  print2c_la_ra("size of long:", sizeof(long));
+  print2c_la_ra("size of float:", sizeof(float));
+  print2c_la_ra("size of double:", sizeof(double));
+}
+//////////////////////////////////////////////////////////////////////////////
+
+
 // logger print
 // a    - code for pass (x > 0) or fail (0)
 // str  - message to print
@@ -23,7 +44,7 @@ void _print(int a, char* str) {
 typedef unsigned char *byte_pointer;
 
 void show_bytes(byte_pointer start, size_t len) {
-  int i;
+  size_t i;
   for (i = 0; i < len; i++) 
     printf(" %.2x", start[i]);
   printf("\n");
@@ -52,10 +73,14 @@ void test__show_bytes(int val) {
   show_float(fval);
   show_pointer(pval);
 }
+
+
+
 //////////////////////////////////////////////////////////////////////////////
 
 int main() {
   test__show_bytes(21928734);
+  show_type_sizes();
 
   return 0;
 }
