187726e0e5b9f231a9e4c69fb5ca46789d88d07d
Adam <asoliyev13@gmail.com>
2022-12-23 22:30:38 -0600

    updated README

--- 1-core-cs/5_operating_systems/code/shell.c
+++ 1-core-cs/5_operating_systems/code/shell.c
@@ -1,7 +1,7 @@
 /*
 Shell
     [DONE] 1. Creating the child process and executing the command in the child
-    2. Providing a history feature
+    [DONE] 2. Providing a history feature
     3. Adding support of input and output redirection
     4. Allowing the parent and child processes to communicate via a pipe
 */
