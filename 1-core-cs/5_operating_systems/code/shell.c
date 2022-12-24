3b429d07b6da4666ea5bc19650f51626d0ac41e5
Adam <asoliyev13@gmail.com>
2022-12-23 22:29:32 -0600

    added history feature using queue

--- 1-core-cs/5_operating_systems/code/shell.c
+++ 1-core-cs/5_operating_systems/code/shell.c
@@ -5,15 +5,6 @@ Shell
     3. Adding support of input and output redirection
     4. Allowing the parent and child processes to communicate via a pipe
 */
-/*
-ls -lah &
-ls -lah
-ls -lah &
-ls -lah
-ls -lah
-ls -lah &
-               commands => [...] => removed the oldest ones if full
-*/
 
 #include <assert.h>
 #include <stdbool.h>
@@ -24,7 +15,70 @@ ls -lah &
 #include <sys/wait.h>
 #include <unistd.h>
 
-#define MAX_LINE 80 // the maximum length commando
+// ------------------------------ DEQUE ------------------------------
+#define QUEUE_SIZE 10
+
+typedef struct {
+    int front;
+    int rear;
+    int count;
+    char *items[QUEUE_SIZE];
+} Queue;
+
+// USAGE
+// Queue q; // history
+// init(&q);
+// enqueue(&q, "item7");
+// printf("Dequeued: %s\n", dequeue(&q));
+
+void init(Queue *q) {
+    q->front = 0;
+    q->rear = 0;
+    q->count = 0;
+}
+
+int is_empty(Queue *q) { return q->count == 0; }
+
+int is_full(Queue *q) { return q->count == QUEUE_SIZE; }
+
+void enqueue(Queue *q, char *item) {
+    if (is_full(q)) {
+        free(q->items[q->front]);
+        q->front = (q->front + 1) % QUEUE_SIZE;
+        q->count--;
+    }
+    char *copy = malloc(strlen(item) + 1);
+    if (copy == NULL) {
+        printf("Failed to allocate memory for queue element!\n");
+        return;
+    }
+    strcpy(copy, item);
+    q->items[q->rear] = copy;
+    q->rear = (q->rear + 1) % QUEUE_SIZE;
+    q->count++;
+}
+
+char *dequeue(Queue *q) {
+    if (is_empty(q)) {
+        printf("Queue is empty!\n");
+        return NULL;
+    }
+
+    char *item = q->items[q->front];
+    q->front = (q->front + 1) % QUEUE_SIZE;
+    q->count--;
+    return item;
+}
+
+void print_queue(Queue *q) {
+    int start = q->front, end = q->rear;
+    for (int i = start; i < end; i++) {
+        printf("%s\n", q->items[i]);
+    }
+}
+// --------------------------- DEQUE END ---------------------------
+
+#define MAX_LINE 80 // the maximum length command
 
 void tokenize(const char *string, char *delimiter, char **tokens,
               size_t *num_tokens, size_t max_tokens_sz) {
@@ -37,7 +91,7 @@ void tokenize(const char *string, char *delimiter, char **tokens,
     strcpy(str_copy, string);
     assert(str_copy != NULL); // ensure str_copy is a valid pointer
 
-    char *token  = strtok(str_copy, delimiter);
+    char *token = strtok(str_copy, delimiter);
     size_t index = 0;
     while (token != NULL) {
         // ensure tokens array is large enough
@@ -53,11 +107,11 @@ void tokenize(const char *string, char *delimiter, char **tokens,
 char **add_null(char **array, size_t array_length, bool concurrent) {
     assert(array != NULL);
     assert(array_length > 0);
-    size_t new_array_length       = array_length + 1;
+    size_t new_array_length = array_length + 1;
     size_t new_array_last_element = array_length;
 
     if (concurrent) { // array has unnecessary '&' at the end
-        new_array_length       = array_length;
+        new_array_length = array_length;
         new_array_last_element = array_length - 1;
     }
     /*
@@ -94,7 +148,7 @@ void run_command(char **args, size_t num_tokens) {
     } else if (child_pid == 0) {
         // child process
         char path_to_program[] = "/bin/";
-        char *program          = args_with_null[0];
+        char *program = args_with_null[0];
         strcat(path_to_program, program);
 
         // Execute the program
@@ -134,9 +188,9 @@ void print_test_result(bool passed, const char *function_name) {
 
 // --------------------------- TESTS ---------------------------
 void test_concurrent_off__add_null() {
-    char *array[]       = {"one", "two", "three"};
+    char *array[] = {"one", "two", "three"};
     size_t array_length = 3;
-    char **new_array    = add_null(array, array_length, false);
+    char **new_array = add_null(array, array_length, false);
 
     assert(new_array[0] == array[0]);
     assert(new_array[3] == NULL);
@@ -145,9 +199,9 @@ void test_concurrent_off__add_null() {
 };
 
 void test_concurrent_on__add_null() {
-    char *array[]       = {"one", "two", "three", "&"};
+    char *array[] = {"one", "two", "three", "&"};
     size_t array_length = 4;
-    char **new_array    = add_null(array, array_length, true);
+    char **new_array = add_null(array, array_length, true);
 
     assert(new_array[0] == array[0]);
     assert(new_array[3] == NULL);
@@ -168,21 +222,27 @@ int main(void) {
     char input[100];
     char *args[(MAX_LINE / 2) + 1]; // command line arguments
     int should_run = 1;             // flag to determine when to exit program
+
+    Queue q; // history
+    init(&q);
+
     while (should_run) {
         printf("osh> ");
         fflush(stdout); // ensure "osh>" is printed
 
         fgets(input, sizeof(input), stdin);
         remove_newline(input);
-        // push input to history deque
+
+        // push input to history
+        enqueue(&q, input);
 
         // check for exit
         if (strcmp(input, "exit") == 0) {
             break;
+        } else if (strcmp(input, "hist") == 0) {
+            print_queue(&q);
+            continue;
         }
-        // else if (strcmp(input, "hist") == 0) {
-        //     printHistory();
-        // }
 
         // Tokenize and put results into args
         size_t num_tokens;
--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # Teach Yourself CS 
 
-## Courses
+## Courses 
 * [MIT 6.851: Advanced Data Structures](https://courses.csail.mit.edu/6.851)
